8943933fae466a15729c3a77d2a18660283429cd
john-mathews <j-mathews@live.com>
2025-08-08 22:07:04 -0400

    Update CollectiblesResource.gd

--- Entities/Pickups/Data/CollectiblesResource.gd
+++ Entities/Pickups/Data/CollectiblesResource.gd
@@ -1,7 +1,14 @@
 class_name Collectible
 extends Resource
 
+enum CollectibleTypes {
+	RESOURCE,
+	CURRENCY,
+	POWERUP
+}
+
 @export var name: String
+@export var type: CollectibleTypes = CollectibleTypes.RESOURCE
 #@export var scene: PackedScene
 @export var value: int
 @export var sprite_path: String
