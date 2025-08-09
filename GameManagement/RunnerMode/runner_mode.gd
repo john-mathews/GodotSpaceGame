128eaa485a7b827e6f5c57b1fbed4ee02321f246
john-mathews <j-mathews@live.com>
2025-08-09 19:06:43 -0400

    add x offset to asteroid spawn

--- GameManagement/RunnerMode/runner_mode.gd
+++ GameManagement/RunnerMode/runner_mode.gd
@@ -13,15 +13,17 @@ func _process(delta: float) -> void:
 		max_distance = player.global_position.x
 		hud.set_distance_label(max_distance)
 
-func getAsteroidSpawnPos():
+func getAsteroidSpawnPos(x_offset: float = 0.0):
 	var point = (Vector2.RIGHT * asteroid_spawn_radius) + player.global_position
-	point.y = randf_range( -400.0,  400.0,)
+	point.y = randf_range( -400.0,  400.0)
+	if x_offset > 0.0:
+		point.x = x_offset
 	return point
 
 func _on_asteroid_spawn_timer_timeout() -> void:
 	var speed_level := int(player.velocity.x/100) + 1
 	for i in range(speed_level):
-		spawn_asteroid(getAsteroidSpawnPos(), initial_spawn_sizes.pick_random(), 1)
+		spawn_asteroid(getAsteroidSpawnPos(i * 100.0), initial_spawn_sizes.pick_random(), 1)
 	var max1 = max(.1, init_max_spawn - speed_level/2.0)
 	var min1 = min(max(.03, init_min_spawn - speed_level/5.0), max1)
 	min_min_time = min(min_min_time, min1)
